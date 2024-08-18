9472c8b87321a6eb63fdbe05df3712ca5ce7648d
hs-anaianzina <alexandranayanzina@gmail.com>
2024-08-17 20:59:52 -0400

    different type of selectors are added

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -17,16 +17,22 @@ describe('Interacting with web elements', {
       // spec config
     },
       ()=> {
+      cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
         //By tag name
+        cy.get('h2').should('have.text','CONTACT US')
         // By attribute name and value
+        cy.get('input[name="first_name"]').type(testData.first_name)
         // By id
+        cy.get('#contact_form').find('input[name="last_name"]').type(testData.last_name)
         // By class
+        cy.get('.feedback-input').eq(2).type(testData.email)
         // By multiple classes
+        cy.get('.row').find('.feedback-input').eq(3).type(testData.comment)
         // By two different attributes
+        cy.get('[type="reset"][value="RESET"]').should('be.visible')
         // By xpath
-
+        cy.xpath('//input[@value="SUBMIT"]').click({force: true})
       })
-
   })
 
 
