6e8831225359036fcc1a47e2d3b0598aa25a2a56
hs-anaianzina <alexandranayanzina@gmail.com>
2024-08-20 22:53:45 -0400

    1. contact-as.js - negative test was added

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -21,7 +21,9 @@ describe('Interacting with web elements', {
         //By tag name
         cy.get('h2').should('have.text','CONTACT US')
         // By attribute name and value
-        cy.get('input[name="first_name"]').type(testData.first_name)
+        cy.get('input[name="first_name"]')
+          .should('have.attr','name','first_name')
+          .type(testData.first_name)
         // By id
         cy.get('#contact_form').find('input[name="last_name"]').type(testData.last_name)
         // By class
@@ -34,7 +36,22 @@ describe('Interacting with web elements', {
         cy.xpath('//input[@value="SUBMIT"]').click({force: true})
         cy.location('href').should('eq','https://webdriveruniversity.com/Contact-Us/contact-form-thank-you.html')
         cy.get('h1').should('have.text','Thank You for your Message!')
+        cy.contains(/^Thank/i)
         cy.get('#fountainG').should('be.visible')
+      })
+
+    it('Submit Contact Us form - negative case - submit empty form', {
+        // spec config
+      },
+      ()=> {
+      const errorMessage = '\n\n\n Error: all fields are required\n Error: Invalid email address\n\n\n'
+        cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+        cy.xpath('//input[@value="SUBMIT"]').click({force: true})
+        cy.location('href').should('eq','https://webdriveruniversity.com/Contact-Us/contact_us.php')
+        cy.contains('Error: all fields are required').should('be.visible')
+        cy.contains('Error: Invalid email address').should('be.visible')
+        cy.get('body').first().should('have.text', errorMessage)
+
       })
   })
 
