0ebae653806877f006d9008caba3944c6b289213
hs-anaianzina <alexandranayanzina@gmail.com>
2024-08-26 18:28:41 -0400

    cypress.yml was updated

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "47teht",
+  projectId: "7ymguf",
   e2e: {
     baseUrl: "https://automationteststore.com",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx, feature}"
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx, feature}",
+    screenshotOnRunFailure: true
   },
 
 });
