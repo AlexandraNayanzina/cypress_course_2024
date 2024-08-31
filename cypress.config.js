dd713a18b745bdbd38888648e72b0ce2a3cdc7a3
hs-anaianzina <alexandranayanzina@gmail.com>
2024-08-31 18:16:43 -0400

    cypress.yml updated

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '7ymguf',
   e2e: {
     baseUrl: "https://automationteststore.com",
     setupNodeEvents(on, config) {
